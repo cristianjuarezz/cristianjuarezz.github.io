c653a09d671882b992c7e534b93fbffba6892231
Cristian Juarez <cristianjuarezdev@gmail.com>
2022-05-09 22:56:53 +0000

    Bow and Quote

---
+++ pages/components/Social/Social.js
@@ -0,0 +1,11 @@
+import styles from '../../../styles/Social.module.sass'
+
+export default function Social(){
+	return (
+		<div className={styles.social}>
+			<a href=""><i>M</i></a>
+			<a href=""><i>G</i></a>
+			<a href=""><i>T</i></a>
+		</div>
+	)
+}
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import styles from '../styles/Home.module.sass'
 import Image from 'next/image'
 import Header from './components/Header/Header'
+import Social from './components/Social/Social'
+import Bow from './components/Figures/Bow'
 
 export default function Home() {
 	return (
@@ -11,7 +13,12 @@ export default function Home() {
 					<h1>Cristian Juarez</h1>
 					<h2>Software Developer</h2>
 					<div className="line"></div>
-					<h6>Coming soon.</h6>
+					<div className="bow">
+						<a href="https://github.com/cristianjuarezz" target="_blank"><Bow/></a>
+					</div>
+					<span className={styles.quote}>I build software solutions from <b>websites</b> to mobile <b>apps</b> with pleasing <b>UI/UX</b> and clever <b>backend</b> design.</span>
+					<Social/>
+					<h6></h6>
 				</article>
 			</section>
 		</main>
