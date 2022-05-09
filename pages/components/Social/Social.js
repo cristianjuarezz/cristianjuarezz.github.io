55af243839c653e967fec0492fedd3c6a862f4b3
Cristian Juarez <84170516+cristianjuarezz@users.noreply.github.com>
2022-05-09 20:38:24 -0300

    Back to img

--- pages/components/Social/Social.js
+++ pages/components/Social/Social.js
@@ -1,12 +1,11 @@
-import Image from 'next/image'
 import styles from '../../../styles/Social.module.sass'
 
 export default function Social(){
 	return (
 		<div className={styles.social}>
-			<a href="https://www.linkedin.com/in/cristian-juarez-dev" target="_blank" rel="noreferrer noopener"><Image src="/social/LinkedIn.png" alt=""/></a>
-			<a href="https://github.com/cristianjuarezz" target="_blank" rel="noreferrer noopener"><Image src="/social/GitHub.png" alt=""/></a>
-			<a href="https://t.me/MrSatoshiDev" target="_blank" rel="noreferrer noopener"><Image src="/social/Telegram.png" alt=""/></a>
+			<a href="https://www.linkedin.com/in/cristian-juarez-dev" target="_blank" rel="noreferrer noopener"><img src="/social/LinkedIn.png" alt=""/></a>
+			<a href="https://github.com/cristianjuarezz" target="_blank" rel="noreferrer noopener"><img src="/social/GitHub.png" alt=""/></a>
+			<a href="https://t.me/MrSatoshiDev" target="_blank" rel="noreferrer noopener"><img src="/social/Telegram.png" alt=""/></a>
 		</div>
 	)
 }
