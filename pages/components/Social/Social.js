dfd8f007a89f979f29e202f60b9d2efd922fbc3b
Cristian Juarez <84170516+cristianjuarezz@users.noreply.github.com>
2022-05-09 20:29:29 -0300

    Fixed Image warning

--- pages/components/Social/Social.js
+++ pages/components/Social/Social.js
@@ -1,11 +1,12 @@
+import Image from 'next/image'
 import styles from '../../../styles/Social.module.sass'
 
 export default function Social(){
 	return (
 		<div className={styles.social}>
-			<a href="https://www.linkedin.com/in/cristian-juarez-dev" target="_blank" rel="noreferrer"><img src="/social/LinkedIn.png" alt=""/></a>
-			<a href="https://github.com/cristianjuarezz" target="_blank" rel="noreferrer"><img src="/social/GitHub.png" alt=""/></a>
-			<a href="https://t.me/MrSatoshiDev" target="_blank" rel="noreferrer"><img src="/social/Telegram.png" alt=""/></a>
+			<a href="https://www.linkedin.com/in/cristian-juarez-dev" target="_blank" rel="noreferrer"><Image src="/social/LinkedIn.png" alt=""/></a>
+			<a href="https://github.com/cristianjuarezz" target="_blank" rel="noreferrer"><Image src="/social/GitHub.png" alt=""/></a>
+			<a href="https://t.me/MrSatoshiDev" target="_blank" rel="noreferrer"><Image src="/social/Telegram.png" alt=""/></a>
 		</div>
 	)
 }
